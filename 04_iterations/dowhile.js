6ad7c8afb88c528aea4d6b3d0f0fdc61ec91b21c
Katabathuni Bose <53533409+kbose8055@users.noreply.github.com>
2024-02-05 09:53:38 +0000

    Do while done

---
+++ 04_iterations/dowhile.js
@@ -0,0 +1,20 @@
+// while loop
+let i=0
+while (i<=10) {
+    console.log(`Value of i: ${i}`);
+    i++
+}
+
+let myHeroes = ["Batman", "Ironman", "Thor", "Hulk", "Spiderman"]
+let myArr = 0
+while (myArr<myHeroes.length) {
+    console.log(`My Hero is: ${myHeroes[myArr]}`);
+    myArr++
+}
+
+// Do while loop
+let score = 11
+do {
+    console.log(`My score: ${score}`);
+    score++
+} while (score<=10);
