749071f3705e80cb7a9f72beb4a3bd40f899c51c
Katabathuni Bose <53533409+codewithbose@users.noreply.github.com>
2024-10-04 10:34:15 +0000

    printing.js

---
+++ Udemy/printing.js
@@ -0,0 +1,6 @@
+console.log("Bose")
+// it has by default line seperator
+
+process.stdout.write("coder")
+process.stdout.write("Bose")
+// it by default doesn't seperates line continously prints output
