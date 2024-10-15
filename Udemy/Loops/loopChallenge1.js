a91ea3f0cbe9a973b600a6b0e97c81ea98ebfad0
Katabathuni Bose <53533409+codewithbose@users.noreply.github.com>
2024-10-15 05:38:06 +0000

    Loop Challenges

---
+++ Udemy/Loops/loopChallenge1.js
@@ -0,0 +1,8 @@
+let sum = 0;
+let i = 0;
+
+while (i<=5) {
+    sum += i;
+    i++
+}
+console.log(sum);
---
+++ Udemy/Loops/loopChallenge2.js
@@ -0,0 +1,8 @@
+let countdown = [];
+let i = 5;
+
+while (i>=1) {
+    countdown.push(i);
+    i--
+}
+console.log(countdown);
