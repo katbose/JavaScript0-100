0dd4ef429f6ed4b49b62f6dce8e9754d1398b440
Katabathuni Bose <53533409+codewithbose@users.noreply.github.com>
2024-10-15 16:25:53 +0000

    loopChallenge5.js

--- Udemy/Loops/loopchallenge4.js
+++ Udemy/Loops/loopchallenge4.js
@@ -1,4 +1,12 @@
 /*Write a do while loop that add numbers from 1 to 3 and stores the result in the variable named 'total' */
 
 let total = 0;
-let num = 0;
+let num = 1;
+
+do {
+    if (num<4) {
+        total += num;
+    }
+    num++
+} while (num<4);
+console.log(total);
