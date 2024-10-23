f6173fd888d9d878154bc167a0c6da8c7ddf096d
Katabathuni Bose <53533409+codewithbose@users.noreply.github.com>
2024-10-23 05:18:41 +0000

    funChallenge1.js

---
+++ Udemy/Functions/funChallenge1.js
@@ -0,0 +1,9 @@
+/*Write a func named 'makeTea', that takes one parameter 'typeOfTea', and returns String like "Making Green Tea" when acalled with 'Green Tea'. Store the result in a variable name "teaOrder" */
+
+//typeOfTea is a parameter or Placeholder
+function makeTea(typeOfTea) {
+    return "Making Green Tea";
+}
+
+let teaOrder = "Green Tea"
+console.log(makeTea(teaOrder)); //teaOrder inside makeTea function is a argument
