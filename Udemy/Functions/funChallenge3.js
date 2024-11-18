20f74b5f624b555661d925f14f0c12cb01b7ca5f
Katabathuni Bose <53533409+codewithbose@users.noreply.github.com>
2024-11-18 17:24:45 +0000

    funChallenge3.js

---
+++ Udemy/Functions/funChallenge3.js
@@ -0,0 +1,9 @@
+/* Write a arrow function names: "calculateTotal" that takes two parameters price & quantity. The func should return the total cost by multiplying the price and quantity. Store the result in variable name "totalCost".*/
+
+const calculateTotal = (Price, Quantity) => {
+    
+    return `Total Cost is: `+ Price*Quantity;
+}
+let totalCost = calculateTotal(4, 5)
+console.log(totalCost);
+
