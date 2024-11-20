ed6315b4046885a48c588721f0668127af98e7f5
Katabathuni Bose <53533409+codewithbose@users.noreply.github.com>
2024-11-20 10:44:43 +0000

    playWithVariables.js

---
+++ Udemy/Coding Exercise 1/playWithVariables.js
@@ -0,0 +1,51 @@
+/*Playing with Variables
+Task:
+Perform the following mathematical operations
+on the provided variables a and b
+Add
+Subtract
+Multiply
+Divide
+Increment
+Decrement
+Reminder
+ */
+
+// **** DO NOT CHANGE the values *****
+const a = 18;
+const b = 24;
+
+// Addition of two values
+function add() {
+    return a+b;
+}
+
+// Subtract small value from larger one
+function subtract() {
+    return b-a;
+}
+
+function multiply() {
+    return a*b;
+}
+
+// Divide larger value by small
+function divide() {
+    return b/a;
+}
+
+// Increase value of a by 1
+function increment() {
+    return a+1;
+}
+
+// Decrease value of b by 1
+function decrement() {
+    return b-1;
+}
+
+// Divide larger value by small to find the reminder
+function reminder() {
+    if(a>b) return a%b;
+    if(b>a) return b%a;
+}
