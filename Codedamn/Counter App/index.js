8583b73c205652c3ada6429599447fa313948274
Katabathuni Bose <53533409+codewithbose@users.noreply.github.com>
2025-01-10 12:08:13 +0000

    counter app updated

--- Codedamn/Counter App/index.js
+++ Codedamn/Counter App/index.js
@@ -1,13 +1,12 @@
 const incrementBtn = document.querySelector("#incrementBtn")
 const decrementBtn = document.querySelector("#decrementBtn")
+const counterEle = document.getElementById("counter")
 let counter = 0
 function incrementCounter() {
-    const counterEle = document.getElementById("counter")
     counter++;
     counterEle.innerText = counter
 }
 function decrementCounter() {
-    const counterEle = document.getElementById("counter")
     counter--;
     counterEle.innerText = counter
 }
